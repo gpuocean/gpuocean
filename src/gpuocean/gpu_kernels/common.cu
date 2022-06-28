d02e5724357199131a5be7f7f2d0def7ed2c88e5
Håvard Heitlo Holm <havard.heitlo.holm@sintef.no>
2022-06-28 11:12:56 +0200

    Bug fix: Now correctly fetching from wind forcing textures

--- src/gpuocean/gpu_kernels/common.cu
+++ src/gpuocean/gpu_kernels/common.cu
@@ -608,10 +608,10 @@ texture<float, cudaTextureType2D> windstress_Y_next;
 /**
   * Returns the wind stress, trilinearly interpolated in space and time
   * @param wind_stress_t_ \in [0, 1] determines the temporal interpolation (0=current, 1=next)
-  * @param ti_ Location of this thread along the x-axis in number of cells (NOTE: half indices)
-  * @param tj_ Location of this thread along the y-axis in number of cells (NOTE: half indices)
-  * @param nx_ Number of cells along x axis
-  * @param ny_ Number of cells along y axis
+  * @param ti_ Location of this thread along the x-axis in number of cells (NOTE: half indices, including ghost cells)
+  * @param tj_ Location of this thread along the y-axis in number of cells (NOTE: half indices, including ghost cells)
+  * @param nx_ Number of cells along x axis including ghost cells
+  * @param ny_ Number of cells along y axis including ghost cells
   */
 __device__ float windStressX(float wind_stress_t_, float ti_, float tj_, int nx_, int ny_) {
     
