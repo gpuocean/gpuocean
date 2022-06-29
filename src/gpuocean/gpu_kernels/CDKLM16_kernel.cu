20ff2895ccfe0ec690e1d4b49c0c061c75f4dd4e
Håvard Heitlo Holm <havard.heitlo.holm@sintef.no>
2022-06-29 15:25:42 +0200

    Moved wind stress texture and texture fetches to separate file

--- src/gpuocean/gpu_kernels/CDKLM16_kernel.cu
+++ src/gpuocean/gpu_kernels/CDKLM16_kernel.cu
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "common.cu"
-
+#include "external_forcing.cu"
 
 texture<float, cudaTextureType2D> angle_tex;
 texture<float, cudaTextureType2D> coriolis_f_tex;
