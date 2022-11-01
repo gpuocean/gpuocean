d1c8577669139e59e96e0b83d7e5bcb88b215488
FlorianBeiser <florian.beiser@sintef.no>
2022-11-01 18:29:39 +0100

    adapting new model error implementation (remains to fix texture values)

--- src/gpuocean/SWEsimulators/CDKLM16pair.py
+++ src/gpuocean/SWEsimulators/CDKLM16pair.py
@@ -26,9 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 #Import packages we need
-from doctest import ELLIPSIS_MARKER
 import numpy as np
-
+from scipy import interpolate 
 
 # Needed for the random perturbation:
 import pycuda.driver as cuda
@@ -41,10 +40,13 @@ class CDKLM16pair():
     Class that solves the SW equations using the Coriolis well balanced reconstruction scheme, as given by the publication of Chertock, Dudzinski, Kurganov and Lukacova-Medvidova (CDFLM) in 2016.
     """
 
-    def __init__(self, sim, slave_sim, 
+    def __init__(self, sim, slave_sim=None, 
                     small_scale_perturbation=False, 
                     small_scale_perturbation_amplitude=None, 
-                    small_scale_perturbation_interpolation_factor=1):
+                    small_scale_perturbation_interpolation_factor=1,
+                    small_scale_perturbation_dx=None,
+                    small_scale_perturbation_dy=None
+                    ):
         """
         Initialization routine
 
@@ -53,11 +55,12 @@ class CDKLM16pair():
         """
         
         # Check whether sims can be combined 
-        assert sim.nx == slave_sim.nx
-        assert sim.ny == slave_sim.ny
-        assert sim.dx == slave_sim.dx
-        assert sim.dy == slave_sim.dy
-        assert sim.t == slave_sim.t
+        if slave_sim is not None:
+            assert sim.nx == slave_sim.nx
+            assert sim.ny == slave_sim.ny
+            assert sim.dx == slave_sim.dx
+            assert sim.dy == slave_sim.dy
+            assert sim.t == slave_sim.t
 
         # Note CDKLM16 instances cannot be copied due to pycuda conflicts,
         # hence we need to initialise two independent simulations outside this class
@@ -71,12 +74,20 @@ class CDKLM16pair():
         if self.l > 0:
             self.level_rescale_factor = sim.children[0].level_rescale_factor
 
-        self.slave_l = self.deepest_level(self.slave_sim, 0)
-        assert self.l > self.slave_l, "Slave should be the coarser simulation"
+        if slave_sim is not None:
+            self.slave_l = self.deepest_level(self.slave_sim, 0)
+            assert self.l > self.slave_l, "Slave should be the coarser simulation"
 
         # Model error instance
         self.soar_q0 = small_scale_perturbation_amplitude
         self.interpolation_factor = small_scale_perturbation_interpolation_factor
+        self.pert_dx = small_scale_perturbation_dx
+        if self.pert_dx is None:
+            self.pert_dx = sim.dx/5
+        self.pert_dy = small_scale_perturbation_dy
+        if self.pert_dy is None:
+            self.pert_dy = sim.dy/5
+
         self.small_scale_model_error = None
         if small_scale_perturbation:
             self.pert_stream = cuda.Stream()
@@ -88,19 +99,18 @@ class CDKLM16pair():
         Instance of OceanNoiseState on the finest level 
         as preparation to sample model error
         """
-        rescale = self.level_rescale_factor**self.l
-        self.fine_NX = np.ceil((self.sim.nx+4) * rescale).astype(np.int32)
-        self.fine_NY = np.ceil((self.sim.ny+4) * rescale).astype(np.int32)
+        self.pert_NX = np.ceil((self.sim.nx+4) * self.sim.dx/self.pert_dx).astype(np.int32)
+        self.pert_NY = np.ceil((self.sim.ny+4) * self.sim.dy/self.pert_dy).astype(np.int32)
 
-        self.fine_dx = self.sim.dx/rescale
-        self.fine_dy = self.sim.dy/rescale
-
-        self.small_scale_model_error = OceanStateNoise.OceanStateNoise(self.sim.gpu_ctx, self.pert_stream, self.fine_NX, self.fine_NY, self.fine_dx, self.fine_dy, \
+        self.small_scale_model_error = OceanStateNoise.OceanStateNoise(self.sim.gpu_ctx, self.pert_stream, self.pert_NX, self.pert_NY, self.pert_dx, self.pert_dx, \
                                                                     self.sim.boundary_conditions, staggered=False, \
                                                                     soar_q0=self.soar_q0, \
                                                                     interpolation_factor=self.interpolation_factor, \
                                                                     angle=Simulator.Simulator.get_texture(self.sim, "angle_tex"), \
                                                                     coriolis_f=Simulator.Simulator.get_texture(self.sim, "coriolis_f_tex"))
+
+        self.small_scale_model_error.bicubicInterpolationEtaKernel = self.small_scale_model_error.kernels.get_function("bicubicInterpolationEta")
+        self.small_scale_model_error.bicubicInterpolationEtaKernel.prepare("iiffiiffffffPiPi")
         
 
     def step(self, t, apply_stochastic_term=False):
@@ -109,7 +119,8 @@ class CDKLM16pair():
         """
         # Stepping forward in time
         self.sim.step(t)
-        self.slave_sim.step(t)
+        if self.slave_sim is not None:
+            self.slave_sim.step(t)
 
         # Model error
         if apply_stochastic_term:
@@ -132,77 +143,93 @@ class CDKLM16pair():
         Adding same realisation of model error to sim and slave sim
         """
         # creating a perturbation in eta
-        #FIXME: Write kernel that only creates perturbation in eta
-        eta_pert = Common.CUDAArray2D(self.pert_stream, self.fine_NX, self.fine_NY, 0,0, np.zeros((self.fine_NY, self.fine_NX)))
-        dummy_hu_pert = Common.CUDAArray2D(self.pert_stream, self.fine_NX, self.fine_NY, 0,0, np.zeros((self.fine_NY, self.fine_NX)))
-        dummy_hv_pert = Common.CUDAArray2D(self.pert_stream, self.fine_NX, self.fine_NY, 0,0, np.zeros((self.fine_NY, self.fine_NX)))
+        eta_pert = Common.CUDAArray2D(self.pert_stream, self.pert_NX, self.pert_NY, 0,0, np.zeros((self.pert_NY, self.pert_NX)))
 
-        dummy_fine_bathymetry = Common.Bathymetry(self.sim.gpu_ctx, self.sim.gpu_stream, self.fine_NX, self.fine_NY, 0, 0, np.ones((self.fine_NY+1, self.fine_NX+1)))
+        self.small_scale_model_error.generateNormalDistribution()
 
-        self.small_scale_model_error.perturbOceanState(eta_pert, dummy_hu_pert, dummy_hv_pert, dummy_fine_bathymetry.Bi, 0.0, ghost_cells_x=0, ghost_cells_y=0)
+        self.small_scale_model_error.soarKernel.prepared_async_call(self.small_scale_model_error.global_size_SOAR, self.small_scale_model_error.local_size, self.pert_stream,
+                                    self.small_scale_model_error.coarse_nx, self.small_scale_model_error.coarse_ny,
+                                    self.small_scale_model_error.coarse_dx, self.small_scale_model_error.coarse_dy,
+
+                                    np.float32(self.small_scale_model_error.soar_q0), self.small_scale_model_error.soar_L,
+                                    np.float32(1.0),
+                                    
+                                    self.small_scale_model_error.periodicNorthSouth, self.small_scale_model_error.periodicEastWest,
+                                    self.small_scale_model_error.random_numbers.data.gpudata, self.small_scale_model_error.random_numbers.pitch,
+                                    self.small_scale_model_error.coarse_buffer.data.gpudata, self.small_scale_model_error.coarse_buffer.pitch,
+                                    np.int32(0))
+
+        self.small_scale_model_error.bicubicInterpolationEtaKernel.prepared_async_call(self.small_scale_model_error.global_size_geo_balance, self.small_scale_model_error.local_size, self.pert_stream,
+                                                                self.small_scale_model_error.nx, self.small_scale_model_error.ny, 
+                                                                self.small_scale_model_error.dx, self.small_scale_model_error.dy,
+                                                                
+                                                                self.small_scale_model_error.coarse_nx, self.small_scale_model_error.coarse_ny,
+                                                                self.small_scale_model_error.coarse_dx, self.small_scale_model_error.coarse_dy,
+                                                                
+                                                                np.float32(self.sim.g), np.float32(np.average(self.sim.f)),
+                                                                np.float32(self.sim.coriolis_beta), np.float32(0.0),
+                                                                
+                                                                self.small_scale_model_error.coarse_buffer.data.gpudata, self.small_scale_model_error.coarse_buffer.pitch,
+                                                                eta_pert.data.gpudata, eta_pert.pitch)
+        
+        # project eta onto levels
+        x0 = np.linspace(0.5*self.pert_dx, (self.pert_NX-0.5)*self.pert_dx, self.pert_NX)
+        y0 = np.linspace(0.5*self.pert_dy, (self.pert_NY-0.5)*self.pert_dy, self.pert_NY)
+        interp = interpolate.interp2d(x0, y0, eta_pert.download(self.small_scale_model_error.gpu_stream), kind="linear")
 
         def perturb_level(level_sim):
             """
             Using eta_pert to generate a perturbation of a certain level
             1 projecting onto certain level
-            2 cutting out local domain
-            3 generating geostrophic balanced perturbation for hu and hv
+            2 generating geostrophic balanced perturbation for hu and hv
             """
-            # NOTE: Some methods still work with ghost cells what is going  to be depreciated 
 
             # projecting onto right level
-            level_NX = np.ceil((self.sim.nx+4) * level_sim.global_rescale_factor).astype(np.int32)
-            level_NY = np.ceil((self.sim.ny+4) * level_sim.global_rescale_factor).astype(np.int32)
-            level_eta_pert = OceanographicUtilities.rescaleMidpoints(eta_pert.data.get(), level_NX, level_NY)[2]
+            global_local_area_x = level_sim.global_local_area[1][1] - level_sim.global_local_area[0][1]
+            global_local_area_y = level_sim.global_local_area[1][0] - level_sim.global_local_area[0][0]
 
-            # Cutting out the relevant eta perturbation
-            loc_eta_pert = level_eta_pert
-            if level_sim.level > 0:
-                fine_y0 = np.round(level_sim.global_local_area[0][0] * (self.sim.ny+4) * level_sim.global_rescale_factor, 3)
-                fine_x0 = np.round(level_sim.global_local_area[0][1] * (self.sim.nx+4) * level_sim.global_rescale_factor, 3)
-                loc_eta_pert = (1 - fine_x0%1 - fine_y0%1 + fine_x0%1*fine_y0%1) * level_eta_pert[int(fine_y0):int(fine_y0+level_sim.ny+4),int(fine_x0):int(fine_x0+level_sim.nx+4)] \
-                                +  (fine_x0%1 - fine_x0%1*fine_y0%1) * level_eta_pert[int(fine_y0):int(fine_y0+level_sim.ny+4),int(fine_x0+1):int(fine_x0+level_sim.nx+5)] \
-                                +  (fine_y0%1 - fine_x0%1*fine_y0%1) * level_eta_pert[int(fine_y0+1):int(fine_y0+level_sim.ny+5),int(fine_x0):int(fine_x0+level_sim.nx+4)] \
-                                +  fine_x0%1*fine_y0%1 * level_eta_pert[int(fine_y0+1):int(fine_y0+level_sim.ny+5),int(fine_x0+1):int(fine_x0+level_sim.nx+5)]
+            x1 = np.linspace((level_sim.global_local_area[0][1]/(global_local_area_x/level_sim.nx) - 1.5)*level_sim.dx, 
+                                (level_sim.global_local_area[1][1]/(global_local_area_x/level_sim.nx) + 1.5)*level_sim.dx, 
+                            level_sim.nx+4)
+            y1 = np.linspace((level_sim.global_local_area[0][0]/(global_local_area_y/level_sim.ny) - 1.5)*level_sim.dy, 
+                                (level_sim.global_local_area[1][0]/(global_local_area_y/level_sim.ny) + 1.5)*level_sim.dy,
+                            level_sim.ny+4)
 
+            parent_eta_pertHOST = interp(x1,y1)
+            parent_eta_pert = Common.CUDAArray2D(self.pert_stream, level_sim.nx, level_sim.ny, 2, 2, np.float32(parent_eta_pertHOST))
 
-            # Generating hu and hv perturbations by geostrophic balance
-            #FIXME: Write kernel that balances the perturbation and then updates the gpudata directly
-            loc_eta_pert = Common.CUDAArray2D(self.pert_stream, level_sim.nx, level_sim.ny, 2, 2, loc_eta_pert)
-            loc_tmp_pert = Common.CUDAArray2D(self.pert_stream, level_sim.nx, level_sim.ny, 2,2, np.zeros((level_sim.ny+4, level_sim.nx+4)))
-            loc_hu_pert = Common.CUDAArray2D(self.pert_stream, level_sim.nx, level_sim.ny, 2,2, np.zeros((level_sim.ny+4, level_sim.nx+4)))
-            loc_hv_pert = Common.CUDAArray2D(self.pert_stream, level_sim.nx, level_sim.ny, 2,2, np.zeros((level_sim.ny+4, level_sim.nx+4)))
-
+            # generating geostrophic balance and update gpudata
+            # FIXME: Write a kernel that uses proper angle and coriolis values! 
             self.small_scale_model_error.geostrophicBalanceKernel.prepared_async_call(self.small_scale_model_error.global_size_geo_balance, self.small_scale_model_error.local_size, self.pert_stream,
                                                               np.int32(level_sim.nx), np.int32(level_sim.ny),
                                                               np.int32(level_sim.dx), np.int32(level_sim.dy),
-                                                              np.int32(2), np.int32(2),
+                                                              np.int32(2.0), np.int32(2.0),
 
-                                                              np.float32(level_sim.g), np.float32(0.0), np.float32(0.0), np.float32(0.0),
+                                                              np.float32(level_sim.g), np.float32(np.average(level_sim.f)), np.float32(level_sim.coriolis_beta), np.float32(0.0),
 
-                                                              loc_eta_pert.data.gpudata, loc_eta_pert.pitch,
+                                                              parent_eta_pert.data.gpudata, parent_eta_pert.pitch,
 
-                                                              loc_tmp_pert.data.gpudata, loc_tmp_pert.pitch,
-                                                              loc_hu_pert.data.gpudata, loc_hu_pert.pitch,
-                                                              loc_hv_pert.data.gpudata, loc_hv_pert.pitch,
+                                                              level_sim.gpu_data.h0.data.gpudata, level_sim.gpu_data.h0.pitch,
+                                                              level_sim.gpu_data.hu0.data.gpudata, level_sim.gpu_data.hu0.pitch,
+                                                              level_sim.gpu_data.hv0.data.gpudata, level_sim.gpu_data.hv0.pitch,
                                                               
                                                               level_sim.bathymetry.Bi.data.gpudata, level_sim.bathymetry.Bi.pitch,
                                                               level_sim.bathymetry.mask_value)
-
-            loc_eta, loc_hu, loc_hv = level_sim.download()
-            level_sim.upload(loc_eta+loc_eta_pert.data.get(), loc_hu+loc_hu_pert.data.get(), loc_hv+loc_hv_pert.data.get())
+          
 
 
         def perturb_all_levels(sim):
             """
             Recursion to add perturbation based on eta_pert onto all levels
             """
+            # FIXME: Avoid multiple interpolation and find smarter way to navigate through tree!!!
             perturb_level(sim)
             if len(sim.children) > 0:
                 perturb_all_levels(sim.children[0]) 
 
         perturb_all_levels(self.sim)
-        perturb_all_levels(self.slave_sim)
+        if self.slave_sim is not None:
+            perturb_all_levels(self.slave_sim)
 
 
     @staticmethod
